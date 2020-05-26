e589aa5179775286b562e50efa38fd640f498733
denis <losangelesdenis2002@gmail.com>
2020-05-27 00:59:25 +0300

    initial commit

--- src/lesson19/HomeWork/Controller.java
+++ src/lesson19/HomeWork/Controller.java
@@ -1,9 +1,25 @@
 package lesson19.HomeWork;
 
 public class Controller {
-    public static void put(Storage storage,File file){
+    public static void put(Storage storage,File file) throws Exception{
+        if(formatChecker(file.getFormat(),storage.getFormatsSupported())==false)
+            throw new Exception("this format is not supported");
+        if(sizeCheker(storage,file.getSize())==false)
+            throw new Exception("not enough space in storage");
+        if(sameFileCheker(storage,file.getId())==false)
+            throw new Exception("File with same id already defined in scope");
+        if(emptyChecker(storage)==true)
+            throw new Exception("No empty space if array");
 
-    }
+
+
+//           for(File filer: storage.getFiles()){
+//             //  if(filer.getId()==file.getId())
+//                   //checker=true;
+//           }
+
+        }
+
     public static void delete(Storage storage,File file){
 
     }
@@ -14,5 +30,50 @@ public class Controller {
 
     public static void transferFile(Storage storageFrom, Storage storageTo,long id){
 
+    }
+    public static File[] addFile(File[] files,File file){
+        for(int i=0;i<files.length;i++)
+        {
+            if(files[i].equals(null))
+               files[i]=file;
+        }
+        return files;
+    }
+    public static boolean emptyChecker(Storage storage){
+        for(File file:storage.getFiles())
+        {
+            if(file.equals(null))
+                return true;
+        }
+        return false;
+    }
+    public static boolean sameFileCheker(Storage storage,long id){
+        for(File file:storage.getFiles())
+        {
+         if(file.getId()==id)
+             return false;
+        }
+        return true;
+    }
+
+    public static boolean sizeCheker(Storage storage,long size){
+    long result=size;
+            for(File file:storage.getFiles())
+            {
+                result+=file.getSize();
+            }
+            if(result>storage.getStorageSize())
+                return false;
+            return true;
+    }
+
+    public static boolean formatChecker(String format,String[] acceptedFormats){
+        for(String string :acceptedFormats)
+        {
+            if(format.equals(string)){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/lesson19/HomeWork/Storage.java
+++ src/lesson19/HomeWork/Storage.java
@@ -6,4 +6,44 @@ public class Storage {
     private String[] formatsSupported;
     private String storageCountry;
     long storageSize;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public File[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(File[] files) {
+        this.files = files;
+    }
+
+    public String[] getFormatsSupported() {
+        return formatsSupported;
+    }
+
+    public void setFormatsSupported(String[] formatsSupported) {
+        this.formatsSupported = formatsSupported;
+    }
+
+    public String getStorageCountry() {
+        return storageCountry;
+    }
+
+    public void setStorageCountry(String storageCountry) {
+        this.storageCountry = storageCountry;
+    }
+
+    public long getStorageSize() {
+        return storageSize;
+    }
+
+    public void setStorageSize(long storageSize) {
+        this.storageSize = storageSize;
+    }
 }

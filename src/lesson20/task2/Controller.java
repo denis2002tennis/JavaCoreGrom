b910f1d40ccb156b5a60bd4492c7c96ed0fa3b1e
denis <losangelesdenis2002@gmail.com>
2020-06-03 01:11:52 +0300

    Test commit

--- src/lesson20/task2/Controller.java
+++ src/lesson20/task2/Controller.java
@@ -1,11 +1,12 @@
 package lesson20.task2;
 
 public class Controller {
-    private Transaction[] transactions=new Transaction[10];
+    private TransactionDAO transactionDAO=new TransactionDAO();
 
-    public Transaction save(Transaction transaction){
 
-        return null;
+    public Transaction save(Transaction transaction) throws Exception{
+
+        return transactionDAO.save(transaction);
     }
     public Transaction[] transactionList(){
         return null;
